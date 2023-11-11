f95f4c117712cc1b5c19ca6f0b2d34097da8f2ac
Xasan Nosirov <148367996+xasannosirov@users.noreply.github.com>
2023-11-11 13:55:41 +0500

    Create 892.py

---
+++ 892.py
@@ -0,0 +1,12 @@
+a = int(input())
+ 
+if a == 1 or a == 2 or a == 12:
+    print("Winter")
+elif a == 5 or a == 3 or a == 4:
+    print("Spring")
+elif a == 6 or a == 7 or a == 8:
+    print("Summer")
+elif a == 9 or a == 10 or a == 11:
+    print("Autumn")
+else:
+    print("Error")
