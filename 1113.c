2ac219fffa8255718d711172deabe3fac75fab37
Xasan Nosirov <148367996+xasannosirov@users.noreply.github.com>
2023-11-11 15:33:02 +0500

    Create 1113.c

---
+++ 1113.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ 
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if(a % b == 0 || b % a == 0)
+        printf("%d", 1);
+    else
+        printf("%d", 666);
+    return 0;
+}
