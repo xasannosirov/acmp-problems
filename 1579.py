2b7eb2dabf383cf90a4b3c4de0f87566e88a6d59
Xasan Nosirov <148367996+xasannosirov@users.noreply.github.com>
2023-11-11 19:15:47 +0500

    Create 1579.py

---
+++ 1579.py
@@ -0,0 +1,8 @@
+lst1 = ['A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','V','X','Y','Z']
+lst2 = ['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','v','x','y','z']
+n = input()
+ 
+if n in lst1 or n in lst2:
+    print("Yes")
+else:
+    print("No")
