cc42feb31440d26833b7d5cc8bdda1c8c50eba30
Xasan Nosirov <148367996+xasannosirov@users.noreply.github.com>
2023-11-11 09:41:19 +0500

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main(){
+    int n;
+    scanf("%d",&n);
+    if (n == 1)
+        printf("198");
+    else if (n == 2)
+        printf("297");
+    else if (n == 3)
+        printf("396");
+    else if (n == 4)
+        printf("495");
+    else if (n == 5)
+        printf("594");
+    else if (n == 6)
+        printf("693");
+    else if (n == 7)
+        printf("792");
+    else if (n == 8)
+        printf("891");
+    else if (n == 9)
+        printf("990");
+     
+    return 0;
+}
