cf24cfd7a398197236c61a45b1002261c1046a29
Xasan Nosirov <148367996+xasannosirov@users.noreply.github.com>
2023-11-11 13:27:49 +0500

    Create 692.py

---
+++ 692.py
@@ -0,0 +1,6 @@
+num = int(input())
+ 
+if num == 1 or num == 2 or num == 4 or num == 8 or num == 16 or num == 32 or num == 64 or num == 128 or num == 256 or num == 512 or num == 1024 or num == 2048 or num == 4096 or num == 8192:
+    print("YES")
+else:
+    print("NO")
