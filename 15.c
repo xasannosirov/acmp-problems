4cb549b819e204fccb1a77d45cb34bb0d7c8c9e6
Xasan Nosirov <148367996+xasannosirov@users.noreply.github.com>
2023-11-24 09:21:23 +0500

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int count = 0;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            int exists;
+            scanf("%d", &exists);
+            count += exists && i < j;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
